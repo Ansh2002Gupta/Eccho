91d04b53015de4a3e0f38577f1d2101bfee06957
AnshGupta <ansh.gupta@unthinkable.co>
2024-07-14 03:01:47 +0530

    Learned to create some api's in node and installed nodemon

--- Server/index.js
+++ Server/index.js
@@ -6,4 +6,20 @@ dotenv.config();
 const app = express();
 const port = process.env.SERVER_PORT;
 
-app.listen(port, () => console.log("Listing on port ", port));
+//to check if server has started.
+app.get("/", (req, res) => {
+  console.log("Everything normal!");
+});
+
+//to load all the chats.
+app.get("/api/chat", (req, res) => {
+  console.log("Chat is loading....");
+  console.log("res:", res);
+});
+
+//to load all the chats of a perticular user.
+app.get("/api/chat/:id", (req, res) => {
+  console.log("Chat with id(?) id loading....");
+});
+
+app.listen(port, () => console.log("Listing to port ", port));
