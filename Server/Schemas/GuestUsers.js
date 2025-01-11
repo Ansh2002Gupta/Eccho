51377b83f9c497da60bdc2a892618bc80f1ce6b0
Ansh Gupta <ansh.gupta@unthinkable.co>
2025-01-11 17:48:35 +0530

    feat: add Contacts and GuestUsers schemas with updated user contact references

---
+++ Server/Schemas/GuestUsers.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const GUEST_USERS = new Schema({
+  Name: { type: String, required: true },
+  Email: { type: String, required: true, unique: true },
+  PhoneNumber: { type: Number, required: true, unique: true },
+  CreatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("GuestUsers", GUEST_USERS);
